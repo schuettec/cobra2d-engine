2e0d4e81bfa216f76fa4eb66d608a3ca0349b8d7
Christopher Schütte <christopher.schuette@remondis.de>
2025-04-15 10:52:20 +0200

    Refactoring

--- src/main/java/com/github/schuettec/cobra2Dexamples/render3d/Demo3D.java
+++ src/main/java/com/github/schuettec/cobra2Dexamples/render3d/Demo3D.java
@@ -6,52 +6,97 @@ import java.util.Properties;
 import com.github.schuettec.cobra2Dexamples.DemoUtils;
 import com.github.schuettec.cobra2d.engine.Cobra2DEngine;
 import com.github.schuettec.cobra2d.engine.Cobra2DProperties;
+import com.github.schuettec.cobra2d.entity.AnimatedEntity3D;
+import com.github.schuettec.cobra2d.entity.Entity3D;
 import com.github.schuettec.cobra2d.entity.camera.BasicRectangleMapCamera;
+import com.github.schuettec.cobra2d.math.Circle;
 import com.github.schuettec.cobra2d.math.Dimension;
+import com.github.schuettec.cobra2d.math.Math2D;
 import com.github.schuettec.cobra2d.math.Point;
+import com.github.schuettec.cobra2d.renderer.RendererAccess;
 import com.github.schuettec.cobra2d.renderer.RendererType;
+import com.github.schuettec.cobra2d.world.WorldAccess;
 
 public class Demo3D {
-  public static void main(String[] args) throws Exception {
-    // 2560x1440, bpp: 24, hz: 165
-    final Properties properties = new Properties();
-    // DemoUtils.windowMode(properties);
-    DemoUtils.windowMode(properties);
-    properties.put(Cobra2DProperties.MAP_UPDATE, "true");
-    properties.put(Cobra2DProperties.RENDERER, RendererType.LIBGDX.toString());
-    properties.put(Cobra2DProperties.CREATE_CONTROLLER, "true");
-    properties.put(Cobra2DProperties.RESOURCE_LOCATION, "classpath");
+	public static void main(String[] args) throws Exception {
+		// 2560x1440, bpp: 24, hz: 165
+		final Properties properties = new Properties();
+		// DemoUtils.windowMode(properties);
+		DemoUtils.windowMode(properties);
+		properties.put(Cobra2DProperties.MAP_UPDATE, "true");
+		properties.put(Cobra2DProperties.RENDERER, RendererType.LIBGDX.toString());
+		properties.put(Cobra2DProperties.CREATE_CONTROLLER, "true");
+		properties.put(Cobra2DProperties.RESOURCE_LOCATION, "classpath");
 
-    Cobra2DEngine engine = new Cobra2DEngine(properties);
-    engine.addModel("brickModel", new URL("resource:models/brick/brick.g3dj"));
-    engine.addModel("brickModelUV", new URL("resource:models/brick/brick-uvmap.g3dj"));
+		Cobra2DEngine engine = new Cobra2DEngine(properties);
+		engine.addModel("brickModel", new URL("resource:models/brick/brick.g3dj"));
+		engine.addModel("brickModelUV", new URL("resource:models/brick/brick-uvmap.g3dj"));
 
-    engine.addModel("brickAnimation", new URL("resource:models/animation/brick-anim.g3dj"));
+		engine.addModel("brickAnimation", new URL("resource:models/animation/brick-anim.g3dj"));
 
-    engine.addModel("uvCube", new URL("resource:models/uvmap/uvcube-blend-exporter.g3dj"));
+		engine.addModel("uvCube", new URL("resource:models/uvmap/uvcube-blend-exporter.g3dj"));
 
-    engine.initialize();
+		engine.initialize();
 
-    Entity3D e1 = new Entity3D("brickModel", new Point(350, 350), 40);
-    e1.setScale(2d);
+		Entity3D e1 = new Entity3D("brickModel", new Point(350, 350), 40) {
+			@Override
+			public void update(WorldAccess worldAccess, float deltaTime) {
+				super.update(worldAccess, deltaTime);
+				setDegrees(Math2D.normalizeAngle(getDegrees() + 1d));
+			}
 
-    AnimatedEntity3D e2 = new AnimatedEntity3D("brickAnimation", new Point(0, 0), 40);
+			@Override
+			public void render(RendererAccess renderer, Point screenTranslation) {
+				super.render(renderer, screenTranslation);
+				Circle collisionShape = getCollisionShapeInWorldCoordinates();
+				renderCircle(collisionShape, renderer, screenTranslation);
+			}
+		};
+		e1.setScale(2d);
 
-    Entity3D e3 = new Entity3D("uvCube", new Point(150, 150), 40);
+		AnimatedEntity3D e2 = new AnimatedEntity3D("brickAnimation", new Point(0, 0), 40);
 
-    Entity3D e4 = new Entity3D("brickModelUV", new Point(250, 250), 40);
-    // e4.setScale(2d);
+		Entity3D e3 = new Entity3D("uvCube", new Point(150, 150), 40) {
+			@Override
+			public void update(WorldAccess worldAccess, float deltaTime) {
+				super.update(worldAccess, deltaTime);
+				setDegrees(Math2D.normalizeAngle(getDegrees() + 1d));
+			}
 
-    BasicRectangleMapCamera camera = new BasicRectangleMapCamera(new Point(0, 0), new Dimension(799, 799), true);
-    camera.setDrawCameraOutline(true);
-    camera.setDrawCollisionShape(true);
-    camera.setDrawEntityPoints(true);
-    engine.setCameraForInput(camera);
-    // Set screen position or the camera is centered automatically
-    // camera.setScreenPosition(new Point(0, 0));
+			@Override
+			public void render(RendererAccess renderer, Point screenTranslation) {
+				super.render(renderer, screenTranslation);
+				Circle collisionShape = getCollisionShapeInWorldCoordinates();
+				renderCircle(collisionShape, renderer, screenTranslation);
+			}
+		};
 
-    engine.addEntity(camera, e1, e2, e3, e4);
-    engine.start();
-  }
+		Entity3D e4 = new Entity3D("brickModelUV", new Point(250, 250), 40) {
+			@Override
+			public void update(WorldAccess worldAccess, float deltaTime) {
+				super.update(worldAccess, deltaTime);
+				setDegrees(Math2D.normalizeAngle(getDegrees() + 1d));
+			}
+
+			@Override
+			public void render(RendererAccess renderer, Point screenTranslation) {
+				super.render(renderer, screenTranslation);
+				Circle collisionShape = getCollisionShapeInWorldCoordinates();
+				renderCircle(collisionShape, renderer, screenTranslation);
+			}
+		};
+		// e4.setScale(2d);
+
+		BasicRectangleMapCamera camera = new BasicRectangleMapCamera(new Point(0, 0), new Dimension(799, 799), true);
+		camera.setDrawCameraOutline(true);
+		camera.setDrawCollisionShape(true);
+		camera.setDrawEntityPoints(true);
+		engine.setCameraForInput(camera);
+		// Set screen position or the camera is centered automatically
+		// camera.setScreenPosition(new Point(0, 0));
+
+		engine.addEntity(camera, e1, e2, e3, e4);
+		engine.start();
+	}
 
 }
---
+++ src/main/java/com/github/schuettec/cobra2d/entity/Entity3D.java
@@ -0,0 +1,110 @@
+package com.github.schuettec.cobra2d.entity;
+
+import static java.util.Objects.isNull;
+
+import java.util.function.Function;
+
+import com.badlogic.gdx.graphics.g3d.Environment;
+import com.badlogic.gdx.graphics.g3d.Model;
+import com.badlogic.gdx.graphics.g3d.ModelBatch;
+import com.badlogic.gdx.graphics.g3d.ModelInstance;
+import com.badlogic.gdx.math.Matrix4;
+import com.badlogic.gdx.math.Quaternion;
+import com.badlogic.gdx.math.Vector3;
+import com.github.schuettec.cobra2d.entity.skills.CircleRenderable;
+import com.github.schuettec.cobra2d.entity.skills.Obstacle;
+import com.github.schuettec.cobra2d.entity.skills.Renderable3D;
+import com.github.schuettec.cobra2d.entity.skills.Updatable;
+import com.github.schuettec.cobra2d.math.Point;
+import com.github.schuettec.cobra2d.renderer.Color;
+import com.github.schuettec.cobra2d.renderer.ModelAccess;
+import com.github.schuettec.cobra2d.renderer.RendererAccess;
+import com.github.schuettec.cobra2d.world.WorldAccess;
+
+public class Entity3D extends BasicCircleEntity implements CircleRenderable, Updatable, Obstacle, Renderable3D {
+
+	private String modelAdress;
+	private ModelInstance modelInstance;
+
+	/**
+	 * The z offset applied before translating to entity position.
+	 */
+	private float zOffset = 10f;
+
+	/**
+	 * The default scaling of the model applied before the entities scaling factor.
+	 */
+	private float scaleOffset = 6f;
+
+	private float rotationX = 0f;
+	private float rotationY = 0f;
+
+	private Vector3 modelTranslation;
+	private Vector3 modelScale;
+	private Quaternion modelRotation;
+
+	public Entity3D(String modelAdress, Point worldCoordinates, double radius) {
+		super(worldCoordinates, radius);
+		this.modelAdress = modelAdress;
+	}
+
+	@Override
+	public void update(WorldAccess worldAccess, float deltaTime) {
+	}
+
+	@Override
+	public void render(RendererAccess renderer, Point screenTranslation) {
+
+	}
+
+	@Override
+	public void render3D(ModelAccess modelAccess, Environment environment, ModelBatch modelBatch,
+	    Function<Point, Point> worldTo3DScreen) {
+
+		if (isNull(modelInstance)) {
+			Model model = modelAccess.getModel(modelAdress);
+			modelInstance = new ModelInstance(model);
+
+			// Position und Skalierung beibehalten
+			this.modelTranslation = new Vector3();
+			modelInstance.transform.getTranslation(modelTranslation); // Position sichern
+
+			this.modelScale = new Vector3();
+			modelInstance.transform.getScale(modelScale); // Skalierung sichern
+
+			this.modelRotation = new Quaternion();
+			modelInstance.transform.getRotation(modelRotation);
+		}
+
+		Point position3d = worldTo3DScreen.apply(getPosition().clone());
+
+		// Transformation mit Rotation aktualisieren
+		modelInstance.transform.idt() // Reset der Transformationsmatrix
+		    .translate(modelTranslation) // Position wiederherstellen
+		    .scale(modelScale.x, modelScale.y, modelScale.z)
+		    .rotate(modelRotation)
+		    .translate(position3d.getFloatX(), position3d.getFloatY(), zOffset)
+		    .scale(scaleOffset, scaleOffset, scaleOffset)
+		    .scale((float) getScale(), (float) getScale(), (float) getScale())
+		    .rotate(Vector3.X, rotationX) // Rotation setzen
+		    .rotate(Vector3.Y, rotationY) // Rotation setzen
+		    .rotate(Vector3.Z, (float) getDegrees()); // Rotation setzen
+
+		modelBatch.render(modelInstance, environment);
+
+		Matrix4 modelTransform = new Matrix4();
+		modelTransform.set(modelInstance.transform);
+		modelTransform.mul(modelAccess.getCamera3D().combined);
+	}
+
+	@Override
+	public int getLayer() {
+		return 0;
+	}
+
+	@Override
+	public Color getDrawColor() {
+		return Color.WHITE;
+	}
+
+}
